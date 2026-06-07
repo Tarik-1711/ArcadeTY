82832e473218de050e18aaeec74ab8994c7a8020
AhmYou-sew <4209@htl.rennweg.at>
2026-06-07 17:06:13 +0200

    prototxp

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        stage.setTitle("ArcadeTY");
+
+        BorderPane flow = new BorderPane();
+        stage.setScene(new Scene(flow, 600, 400));
+
+        Label title = new Label("ArcadeTY");
+
+        Button flappybird = new Button("FLAPPYBIRD");
+        Button tictacto = new Button("TIC TAC TOE");
+
+        flow.setTop(title);
+        flow.setCenter(flappybird);
+        flow.setCenter(tictacto);
+        flow.getChildren().add(flappybird);
+
+
+        stage.show();
+    }
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
