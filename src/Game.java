3faaef960109d6cff22d73bf465ee061d56b5955
AhmYou-sew <4209@htl.rennweg.at>
2026-06-07 17:26:44 +0200

    Game Klasse

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,9 @@
 import javafx.scene.image.Image;
 
 public abstract class Game {
-    public String title;
-    public Image image;
-    public String beschreibung;
+    private String title;
+    private Image image;
+    private String beschreibung;
 
     public Game(String title, Image image) {
         this.title = title;
---
+++ src/GameCard.java
@@ -0,0 +1,3 @@
+public class GameCard {
+    private
+}
