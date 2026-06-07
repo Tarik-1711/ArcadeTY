977b55c86a029bb38eebf0a92fbede08fb325e72
trgot <tariktrgo22@gmail.com>
2026-06-07 18:14:20 +0200

    TicTacToeGame.java erstellt (ist das Spiel an sich)

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TicTacToeGame extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+
+}
