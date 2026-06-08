d1dbc4c6c1870cfa17d5d00598f372f49050d334
AhmYou-sew <4209@htl.rennweg.at>
2026-06-08 10:07:33 +0200

    unnötige Klasse gelöscht und play korrigiert

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,9 +1,17 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class TicTacToe extends Game{
-    public static void main(String[] args) {
-
-    }
+    private char[][] board= new char[3][3];
+    private Button[][] btn= new Button[3][3];
+    private Label status= new Label("X ist dran");
+    private char current= 'X';
 
     public TicTacToe(String title, Image image) {
         super(title, image);
@@ -15,6 +23,22 @@ public class TicTacToe extends Game{
 
     @Override
     public void play() {
-        TicTacToeGame.startGame();
+        start(new Stage());
+    }
+
+
+
+    public void start(Stage stage){
+        BorderPane root= new BorderPane();
+        GridPane gd= new GridPane();
+        stage.setScene(new Scene(root, 600, 400));
+        gd.setAlignment(Pos.CENTER);
+        stage.setTitle("TicTacToe");
+        Label titleH= new Label("Tic-Tac-Toe");
+        titleH.setStyle("-fx-font-size: 32px; -fx-text-alignment: center; -fx-alignment: center;");
+        BorderPane.setAlignment(titleH,Pos.CENTER);
+        root.setTop(titleH);
+        root.setCenter(gd);
+        stage.show();
     }
 }
