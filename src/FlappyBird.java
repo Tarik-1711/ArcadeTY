884fb228e47676b6859f6200d6298aac12948ffb
AhmYou-sew <4209@htl.rennweg.at>
2026-06-13 12:03:39 +0200

    something

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,8 +1,16 @@
+import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class FlappyBird extends Game{
     private Rectangle player;
+    private Stage stage = new Stage();
+    Button jump = new Button("Jump");
+    Boolean jumped = false;
 
     public FlappyBird() {
         super("FlapppyBird", "/resources/flappybird.png", "flappybird", Color.SEAGREEN);
@@ -11,6 +19,50 @@ public class FlappyBird extends Game{
     @Override
     public void play() {
         System.out.println("FlappyBird");
+        Pane p = new Pane();
+        stage.setScene(new Scene(p, 600, 400));
+        stage.setTitle("FlappyBird wow");
+
+        createPlayer();
+        p.getChildren().add(player);
+
+        jump.setOnAction(e -> {
+            player.setLayoutY(player.getLayoutY() - 60);
+            jumped = true;
+        });
+
+        p.getChildren().add(jump);
+
+        stage.show();
+
+        gameLoop();
     }
+
+    public void createPlayer(){
+        player = new Rectangle();
+        player.setArcHeight(20);
+        player.setArcWidth(20);
+
+        player.setHeight(50);
+        player.setWidth(80);
+
+        player.setFill(Color.YELLOW);
+    }
+
+    public void gameLoop() {
+        AnimationTimer loop = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                updatePlayer();
+            }
+        };
+        loop.start();
+    }
+
+    public void updatePlayer() {
+        player.setLayoutY(player.getLayoutY()+1);
+    }
+
+
 
 }
